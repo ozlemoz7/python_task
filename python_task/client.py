5d4b15f683b11dabc3a4dac4d1a59036d1dc20c4
Özlem <110242327+ozlemoz7@users.noreply.github.com>
2023-03-01 20:32:22 +0300

    Update client.py

--- python_task/client.py
+++ python_task/client.py
@@ -1,79 +1,50 @@
-import os
 import argparse
-import requests
-import pandas as pd
 import datetime
+import pandas as pd
+import requests
 
-# Define command line arguments
+# Parse command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('-k', '--keys', nargs='+', default=[], help='list of keys to include as extra columns')
-parser.add_argument('-c', '--colored', action='store_true', help='color each row based on age of hu')
+parser.add_argument('-k', '--keys', nargs='+', help='Additional columns to include in Excel file')
+parser.add_argument('-c', '--colored', action='store_true', default=True, help='If True, color rows based on hu value')
 args = parser.parse_args()
 
-# Read vehicle data from file
-data = pd.read_csv('vehicles.csv')
+# Get current date in ISO format for Excel file name
+current_date_iso_formatted = datetime.datetime.now().strftime('%Y-%m-%d')
 
-# Authenticate with API
-auth_url = 'https://api.baubuddy.de/index.php/login'
-auth_payload = {'username': '365', 'password': '1'}
-auth_headers = {
-    'Authorization': 'Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz',
-    'Content-Type': 'application/json'
-}
-auth_response = requests.post(auth_url, json=auth_payload, headers=auth_headers)
-auth_response.raise_for_status()
-access_token = auth_response.json()['oauth']['access_token']
+# Read CSV file containing vehicle information
+with open('vehicles.csv', 'r') as f:
+    csv_data = f.read()
 
-# Get vehicle data from API
-api_url = 'https://api.baubuddy.de/dev/index.php/v1/vehicles/select/active'
-api_headers = {'Authorization': f'Bearer {access_token}'}
-api_response = requests.post(api_url, json=data.to_dict(orient='records'), headers=api_headers)
-api_response.raise_for_status()
-api_data = pd.DataFrame(api_response.json()['data'])
+# Make POST request to server
+headers = {'Content-type': 'text/csv', 'Authorization': 'Bearer bf427ec8a565621763c113b4a1e1dd9f52338e48'}
+response = requests.post('https://api.baubuddy.de/dev/index.php/login', headers=headers, data=csv_data)
 
-# Filter out resources without hu field
-api_data = api_data.dropna(subset=['hu'])
+# Convert response data to pandas DataFrame and sort by gruppe column
+response_json = response.json()
+df = pd.DataFrame(response_json)
+df = df.sort_values('gruppe')
 
-# Resolve color codes for labelIds
-if 'labelIds' in api_data.columns:
-    label_id_url = 'https://api.baubuddy.de/dev/index.php/v1/labels/{}'
-    color_codes = []
-    for label_ids in api_data['labelIds']:
-        if pd.isna(label_ids):
-            color_codes.append('')
-        else:
-            label_id_list = [int(x) for x in label_ids.split(',')]
-            label_colors = []
-            for label_id in label_id_list:
-                label_response = requests.get(label_id_url.format(label_id), headers=api_headers)
-                label_response.raise_for_status()
-                label_data = label_response.json()['data']
-                if 'colorCode' in label_data and label_data['colorCode']:
-                    label_colors.append(label_data['colorCode'])
-            color_codes.append(','.join(label_colors))
-    api_data['labelColors'] = color_codes
+# Define columns to include in Excel file
+columns_to_include = ['rnr']
+if args.keys is not None:
+    columns_to_include += args.keys
+if 'labelIds' in columns_to_include:
+    columns_to_include.append('colorCode')
+df = df[columns_to_include]
 
-# Add extra columns based on input keys
-if args.keys:
-    for key in args.keys:
-        if key in api_data.columns:
-            data[key] = api_data[key]
-
-# Color rows based on age of hu
+# Color rows based on hu value if -c flag is True
 if args.colored:
-    today = pd.Timestamp.now().date()
-    colors = []
-    for index, row in api_data.iterrows():
-        hu_date = pd.to_datetime(row['hu'], format='%Y-%m-%d').date()
-        if (today - hu_date).days <= 90:
-            colors.append('#007500')
-        elif (today - hu_date).days <= 365:
-            colors.append('#FFA500')
+    def color_row(row):
+        if row['hu'] <= 3:
+            return 'background-color: #007500'
+        elif row['hu'] <= 12:
+            return 'background-color: #FFA500'
         else:
-            colors.append('#b30000')
-    api_data.style.apply(lambda x: f"background-color: {colors[x.name]}", axis=1)
+            return 'background-color: #b30000'
 
-# Sort rows by gruppe field
-api_data = api_data.sort_values('gruppe')
+    df = df.style.applymap(color_row, subset=pd.IndexSlice[:, ['hu']])
 
-# Create output
+# Write DataFrame to Excel file
+file_name = f'vehicles_{current_date_iso_formatted}.xlsx'
+df.to_excel(file_name, index=False)
